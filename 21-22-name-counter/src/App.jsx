662359c5e12e12b6940208f783b51bc74b7cfde9
Web Dev Simplified <kyle@webdevsimplified.com>
2022-11-01 13:33:24 -0500

    Update Project 21-22

--- 21-22-name-counter/src/App.jsx
+++ 21-22-name-counter/src/App.jsx
@@ -1,22 +1,8 @@
-import { useState } from "react"
+import { FunctionComponent } from "./FunctionComponent"
+import { ClassComponent } from "./ClassComponent"
 
 function App() {
-  const [age, setAge] = useState(0)
-  const [name, setName] = useState("")
-
-  return (
-    <div>
-      <input type="text" value={name} onChange={e => setName(e.target.value)} />
-      <br />
-      <br />
-      <button onClick={() => setAge(a => a - 1)}>-</button>
-      {age}
-      <button onClick={() => setAge(a => a + 1)}>+</button>
-      <br />
-      <br />
-      My name is {name} and I am {age} years old.
-    </div>
-  )
+  return <FunctionComponent />
 }
 
 export default App
---
+++ 21-22-name-counter/src/ClassComponent.jsx
@@ -0,0 +1,47 @@
+import React from "react"
+
+export class ClassComponent extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      age: 0,
+      name: "",
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <input
+          type="text"
+          value={this.state.name}
+          onChange={e => this.setState({ name: e.target.value })}
+        />
+        <br />
+        <br />
+        <button
+          onClick={() =>
+            this.setState(state => {
+              return { age: state.age - 1 }
+            })
+          }
+        >
+          -
+        </button>
+        {this.state.age}
+        <button
+          onClick={() =>
+            this.setState(state => {
+              return { age: state.age + 1 }
+            })
+          }
+        >
+          +
+        </button>
+        <br />
+        <br />
+        My name is {this.state.name} and I am {this.state.age} years old.
+      </div>
+    )
+  }
+}
