21306f08f597b6c33f424ada2e3cbd1c6e81c4dc
Web Dev Simplified <kyle@webdevsimplified.com>
2023-02-14 14:17:40 -0600

    Finish Projects

--- 40-41-simple-todo-list/after/src/App.jsx
+++ 40-41-simple-todo-list/after/src/App.jsx
@@ -3,8 +3,8 @@ import "./styles.css"
 import { TodoItem } from "./TodoItem"
 
 function App() {
-  const [todos, setTodos] = useState([])
   const [newTodoName, setNewTodoName] = useState("")
+  const [todos, setTodos] = useState([])
 
   function addNewTodo() {
     if (newTodoName === "") return
@@ -12,52 +12,41 @@ function App() {
     setTodos(currentTodos => {
       return [
         ...currentTodos,
-        { id: crypto.randomUUID(), name: newTodoName, completed: false },
+        { name: newTodoName, completed: false, id: crypto.randomUUID() },
       ]
     })
     setNewTodoName("")
   }
 
-  function toggleTodo(id, checked) {
+  function toggleTodo(todoId, completed) {
     setTodos(currentTodos => {
       return currentTodos.map(todo => {
-        if (todo.id === id) return { ...todo, completed: checked }
+        if (todo.id === todoId) return { ...todo, completed }
+
         return todo
       })
     })
   }
 
-  function deleteTodo(id) {
+  function deleteTodo(todoId) {
     setTodos(currentTodos => {
-      return currentTodos.filter(todo => todo.id !== id)
+      return currentTodos.filter(todo => todo.id !== todoId)
     })
   }
 
   return (
-    <div>
+    <>
       <ul id="list">
-        {todos.map(todo => (
-          <TodoItem
-            {...todo}
-            key={todo.id}
-            toggleTodo={toggleTodo}
-            deleteTodo={deleteTodo}
-          />
-          // <li key={todo.id} className="list-item">
-          //   <label className="list-item-label">
-          //     <input
-          //       type="checkbox"
-          //       data-list-item-checkbox
-          //       checked={todo.completed}
-          //       onChange={e => toggleTodo(todo.id, e.target.checked)}
-          //     />
-          //     <span data-list-item-text>{todo.name}</span>
-          //   </label>
-          //   <button data-button-delete onClick={() => deleteTodo(todo.id)}>
-          //     Delete
-          //   </button>
-          // </li>
-        ))}
+        {todos.map(todo => {
+          return (
+            <TodoItem
+              key={todo.id}
+              {...todo}
+              toggleTodo={toggleTodo}
+              deleteTodo={deleteTodo}
+            />
+          )
+        })}
       </ul>
 
       <div id="new-todo-form">
@@ -70,7 +59,7 @@ function App() {
         />
         <button onClick={addNewTodo}>Add Todo</button>
       </div>
-    </div>
+    </>
   )
 }
 
--- 40-41-simple-todo-list/after/src/TodoItem.jsx
+++ 40-41-simple-todo-list/after/src/TodoItem.jsx
@@ -1,16 +1,16 @@
-export function TodoItem({ id, name, completed, deleteTodo, toggleTodo }) {
+export function TodoItem({ id, name, completed, toggleTodo, deleteTodo }) {
   return (
     <li className="list-item">
       <label className="list-item-label">
         <input
+          checked={completed}
           type="checkbox"
           data-list-item-checkbox
-          checked={completed}
           onChange={e => toggleTodo(id, e.target.checked)}
         />
         <span data-list-item-text>{name}</span>
       </label>
-      <button data-button-delete onClick={() => deleteTodo(id)}>
+      <button onClick={() => deleteTodo(id)} data-button-delete>
         Delete
       </button>
     </li>
