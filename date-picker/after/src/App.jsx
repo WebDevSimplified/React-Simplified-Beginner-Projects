e8f4f823b32a9b982e75d802ef7e5df8a2c7379c
Web Dev Simplified <kyle@webdevsimplified.com>
2023-07-12 09:10:42 -0500

    Remove Date Picker

--- date-picker/after/src/App.jsx
+++
@@ -1,8 +0,0 @@
-import { useState } from "react"
-import { DatePicker } from "./DatePicker"
-import "./styles.css"
-
-export default function App() {
-  const [value, setValue] = useState(new Date())
-  return <DatePicker value={value} onChange={setValue} />
-}
--- date-picker/before/README.md
+++
@@ -1,4 +0,0 @@
-<!-- TODO: Fill out -->
-
-<!-- Make it first be a controlled input and then as a bonus make it also accept a ref as an uncontrolled input (Actually don't do the ref thing until the advanced course -->
-<!-- Maybe mention date-fns  -->
